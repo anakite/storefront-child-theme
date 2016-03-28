6e5ec20ae4496bdb4928e8be1730c31100276a92
anakite <matthew@anakite.com>
2016-03-28 11:27:38 -0400

    Added files via upload

--- functions.php
+++ functions.php
@@ -22,5 +22,15 @@ function sf_child_theme_dequeue_style() {
 /**
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
+function enqueue_child_theme_styles() {
 
+//enqueue my child theme stylesheet
+wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('theme') );
+
+//enqueue bootstrap in the child theme 
+wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.min.js', array('jquery'), NULL, true);
+wp_enqueue_style('bootstrap-css', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css', false, NULL, 'all');
+
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 
